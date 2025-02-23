e8b01df85f614d7802ce5db558c013d2e44e910f
Vanadium <2683795499@qq.com>
2025-02-23 21:51:02 +0800

    add lab 0~2 and hw 1~2

---
+++ hws/hw01/draft.py
@@ -0,0 +1,24 @@
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    count = 0
+    while n > 0:
+        count += 1 
+        n //= 10
+
+    return count % 2 == 0
+
+print(double_eights(88))
